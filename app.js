26381d7c592ba5c5364f180beb1cdd8f56d96508
EAS-Clark <98745819+EAS-Clark@users.noreply.github.com>
2022-04-19 10:47:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ const fileUpload = require('express-fileupload');
 const path = require('path');
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.text());
 app.use(fileUpload());
+app.use(express.raw({ type: 'image/*', limit: '5mb' }));
+
+app.post('/api/single-file', (req, res) => {
+  const contentType = req.header('content-type');
+  if (contentType.includes('text/plain')) {
+      res.set('Content-Type', 'text/plain');
+      res.send(req.body);
+  } else if (contentType.includes('multipart/form-data')) {
+      const f = req.files.myfile;
+      res.set('Content-Type', 'text/html');
+      f.mv('./uploads/' + f.name);
+      res.send(`${f.name}, ${f.size}, ${f.mimetype}`);
+  } else {
+      res.set('Content-Type', contentType);
+      res.send(req.body);
+  }
+});
+
 
 app.get('/', (req, res) => {
-  res.send('Hello from my app');
+  res.send('Hello from my appHi');
 });
 
 app.listen(3000, () => {

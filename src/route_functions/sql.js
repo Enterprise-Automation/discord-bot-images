59e4dcdd1581565341982da39b78a4e742877134
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-04-26 17:31:37 +0100

    added more valdation to upload

--- src/route_functions/sql.js
+++ src/route_functions/sql.js
@@ -86,6 +86,19 @@ exports.func = req => {
         break;
       case "upload":
 
+        if (params[2] == null){
+          reject({ "status": "failed", "status_message": "sending back image", "discord_message": "missing params url" });
+          break;
+        }
+        if (params[3] == null){
+          reject({ "status": "failed", "status_message": "sending back image", "discord_message": "missing params name" });
+          break;
+        }
+        if (params[4] == null){
+          reject({ "status": "failed", "status_message": "sending back image", "discord_message": "missing params tag" });
+          break;
+        }
+
         query = `SELECT * FROM image_HTML_URl WHERE HTML_URL=?`;
 
         connection.query(query, params[2], function (err, result, fields) {
@@ -93,7 +106,7 @@ exports.func = req => {
             reject(err)
           }
           try {
-            resolve({ "status": "failed", "status_message": "sending back image", "discord_message": "image already in saved" + "\nimage is saved as;\nid: " + result[0].id + " name: " + result[0].Name_of_image });
+            reject({ "status": "failed", "status_message": "sending back image", "discord_message": "image already in saved" + "\nimage is saved as;\nid: " + result[0].id + " name: " + result[0].Name_of_image });
           } catch (error) {
 
             query = `SELECT * FROM image_HTML_URl WHERE Name_of_image=?`;
@@ -103,22 +116,23 @@ exports.func = req => {
                 reject(err)
               }
               try {
-                resolve({ "status": "failed", "status_message": "sending back image", "discord_message": "image already in saved urnder that name" + "\nimage is saved as;\nid: " + result[0].id + " name: " + result[0].Name_of_image });
+                reject({ "status": "failed", "status_message": "sending back image", "discord_message": "image already in saved urnder that name" + "\nimage is saved as;\nid: " + result[0].id + " name: " + result[0].Name_of_image });
               } catch (error) {
 
                 query = `INSERT INTO image_HTML_URl 
                 (HTML_URL, Name_of_image, tag) 
                 VALUES
                   (?, ?, ?)`;
-  
-              connection.query(query, [params[2], params[3], params[4]], function (err, result, fields) {
-                if (err) {
-                  reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "Failed to upload image (Url could be to big)" })
-                }
-                resolve({ "status": "success", "status_message": "sending back image", "discord_message": "Upload image. id: " + result.insertId + " Name: " + params[3] });
-              });
-              }});
-            
+
+                connection.query(query, [params[2], params[3], params[4]], function (err, result, fields) {
+                  if (err) {
+                    reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "Failed to upload image (Url could be to big)" })
+                  }
+                  resolve({ "status": "success", "status_message": "sending back image", "discord_message": "Upload image. id: " + result.insertId + " Name: " + params[3] });
+                });
+              }
+            });
+
 
 
           }

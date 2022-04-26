dcedd5bd18fa4dabb73fe77b7fe94c97e1f5bc71
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-04-26 14:50:48 +0100

    search by url

--- src/route_functions/sql.js
+++ src/route_functions/sql.js
@@ -61,11 +61,24 @@ exports.func = req => {
             } catch (error) {
               reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "Can't find a image with the name of " + params[3] });
             }
-
-
           });
+
+        } else if (params[2].toLowerCase() === "url") {
+
+          query = `SELECT * FROM image_HTML_URl WHERE HTML_URL=?`;
+          connection.query(query, params[3], function (err, result, fields) {
+            if (err) {
+              reject(err)
+            }
+            try {
+              resolve({ "status": "success", "status_message": "sending back image", "discord_message": "image is saved as;\nid: " + result[0].id + " name: " + result[0].Name_of_image });
+            } catch (error) {
+              reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "Can't find a image with that url" });
+            }
+          });
+
         } else {
-          reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "You only search by name or id" });
+          reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "You only search by name, url or id" });
 
         }
 

c2df2bff3202d043c14dc9d0f89e2abc7c18fb61
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-04-28 14:38:15 +0100

    updated stats

--- src/actions/stats.js
+++ src/actions/stats.js
@@ -1,61 +1,61 @@
 const process = require('process');
 const os = require('os');
+const disk = require('diskusage');
 
 let query = "";
 module.exports = function (connection, params, resolve, reject) {
 
 
     const memoryUsage = process.memoryUsage().heapUsed / 1024 / 1024;
-    let memoryUsageRound = Math.round(memoryUsage * 100) / 100;
-
-   // console.log(memoryUsageRound);
 
 
     var usage = process.cpuUsage();
 
     usage = process.cpuUsage(usage);
-    /*
-        Hostname: 0d29b856116d
-        Total CPUs: 8
-        Total Memory: 18003.51MB
-        Free Memory: 15026.67MB
-        Platform: linux #1 SMP Mon Nov 8 10:21:19 UTC 2021
-        System Uptime: 631831.77 seconds
-        Bot Uptime: 13.87 seconds
-        Disk Space: 58.4G
-    
-        This will return cpus, memory, free memory, disk, free disk and uptime in a nice format
-    */
 
-      const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`
+    const formatMemoryUsageMB = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`
+    const formatMemoryUsageGB = (data) => `${Math.round(((data / 1024 / 1024 * 100) / 100 ) / 1024 / 1024 * 100) / 100} GB`
 
+    const memoryData = process.memoryUsage();
+    const os = require('os');
+  
 
+    var ut_sec = os.uptime();
+    var ut_min = ut_sec/60;
+    var ut_hour = ut_min/60;
+       
+    ut_sec = Math.floor(ut_sec);
+    ut_min = Math.floor(ut_min);
+    ut_hour = Math.floor(ut_hour);
+      
+    ut_hour = ut_hour%60;
+    ut_min = ut_min%60;
+    ut_sec = ut_sec%60;
+      
+    let diskFree;
+    let diskTotal;
 
- 
+    disk.check('/', function(err, info) {
+        diskFree = info.free;
+        diskTotal = info.total;
+    });
 
-    const memoryData = process.memoryUsage()
-
-    let dfmemoryUsage = 
-        
-    
-
-  //  console.log(dfmemoryUsage)
-
-
-
-
-    //  console.log(process.resourceUsage());
+    console.log(formatMemoryUsageMB(diskTotal));
 
 
-
-
-
-    resolve({ "status": "success", "status_message": "sending back image", "discord_message": `rss: ${formatMemoryUsage(memoryData.rss)} Total memory allocated for the process execution
-    heapTotal: ${formatMemoryUsage(memoryData.heapTotal)}  Total size of the allocated heap
-    heapUsed: ${formatMemoryUsage(memoryData.heapUsed)} Actual memory used during the execution
+    resolve({
+        "status": "success", "status_message": "sending back image", "discord_message": `rss: ${formatMemoryUsageMB(memoryData.rss)} Total memory allocated for the process execution
+    heapTotal: ${formatMemoryUsageMB(memoryData.heapTotal)}  Total size of the allocated heap
+    heapUsed: ${formatMemoryUsageMB(memoryData.heapUsed)} Actual memory used during the execution
     cpu: ${usage.user} Mhz cpu used during the execution
-    CPUS:" ${os.cpus().length}
-    totalmem: ${formatMemoryUsage(os.totalmem())}
-    cfreemem: ${formatMemoryUsage(os.freemem())}` });
+    CPUS: ${os.cpus().length}
+    totalmem: ${formatMemoryUsageMB(os.totalmem())}
+    cfreemem: ${formatMemoryUsageMB(os.freemem())}
+    Up time: ${ut_hour} Hour(s) ${ut_min} minute(s) and ${ut_sec} second(s)
+    Disk free ${formatMemoryUsageGB(diskFree)}
+    Disk total ${formatMemoryUsageGB(diskTotal)}
+    Disk used ${formatMemoryUsageGB(diskTotal - diskFree)}
+    `
+    });
 
 }

bad93aba614f2dde37c7f5c86e758d75f49b4dec
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-05-03 11:46:57 +0100

    Revert "1"
    This reverts commit 50691b142f410c8fdc5e07a6102977eeb0c081de.

--- src/actions/random.js
+++ src/actions/random.js
@@ -1,10 +1,9 @@
 
 let query = "";
-let randomNumber;
 module.exports = function (connection, params, resolve, reject) {
 
 
-  console.log(params);
+  console.log (params);
   if (params[2] == '*') {
     query = `SELECT * FROM image_HTML_URl`;
 
@@ -15,7 +14,7 @@ module.exports = function (connection, params, resolve, reject) {
       }
 
       try {
-        randomNumber = getRandomInt(0, result.length - 1);
+        let randomNumber = getRandomInt(0, result.length - 1);
         resolve({ "status": "success", "status_message": "sending back image", "discord_message": result[randomNumber].HTML_URL });
       } catch (error) {
         reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "failed to find images with tags" });
@@ -23,23 +22,23 @@ module.exports = function (connection, params, resolve, reject) {
 
 
     });
-
+ 
   } else {
     query = `SELECT * FROM image_HTML_URl WHERE tag=?`
 
     query = `
     SELECT * FROM image_HTML_URl
-    WHERE Name_of_image LIKE ?`;
+    WHERE tag LIKE ?`;
 
 
     connection.query(query, `%${params[2]}%`, function (err, result, fields) {
-
+      
       if (err) {
         reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "failed to find images with the tag of " + params[2] + "\ntry the command !image tags\nor you can try !image random *" });
       }
 
       try {
-        randomNumber = getRandomInt(0, result.length - 1);
+        let randomNumber = getRandomInt(0, result.length - 1);
         resolve({ "status": "success", "status_message": "sending back image", "discord_message": result[randomNumber].HTML_URL });
       } catch (error) {
         reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "failed to find images with the tag of " + params[2] });

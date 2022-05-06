8ed594ae4f22740414c0d3569b1daa512aebb973
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-05-06 10:55:56 +0100

    new way

--- src/route_functions/images.js
+++ src/route_functions/images.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const {getAll} = require('../controllers/images.controller')
+const { getAll } = require('../controllers/images.controller')
 
 
 var connection = mysql.createConnection({
@@ -11,11 +11,98 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-exports.get = req => {
-    return new Promise(async (resolve, reject) => {
-        let rows = await getAll()
-        resolve(rows)
-    });
+exports.get = async req => {
+
+    switch (req.headers.action) {
+
+        case "get":
+
+            return new Promise(async (resolve, reject) => {
+                let rows = await getAll()
+                resolve(rows)
+            });
+  
+            break;
+        case "search":
+
+            if (params[2].toLowerCase() === "id") {
+
+                try {
+                    let row = await getByID(params[3]);
+                   
+                    resolve({ "status": "success", "status_message": "sending back image", "discord_message": '' + row.HTML_URL });
+                } catch (err) {
+                    reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "Can't find a image with the id of " + params[3] });
+                }
+        
+        
+            } else if (params[2].toLowerCase() === "name") {
+        
+                try {
+             
+                    let rows = await getByNameLike(params[3]);
+        
+                    randomInt = getRandomInt(0, rows.length -1 );
+        
+                    resolve({ "status": "success", "status_message": "sending back image", "discord_message": '' + rows[randomInt].HTML_URL });
+        
+                } catch (err) {
+                    reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "Can't find a image with the name of " + params[3] });
+                }
+        
+        
+            } else if (params[2].toLowerCase() === "url") {
+        
+                try {
+                    let row = await getByUrl(params[3]);
+                    resolve({ "status": "success", "status_message": "sending back image", "discord_message": "image is saved as;\nid: " + row.id + " name: " + row.Name_of_image });
+                } catch (err) {
+                    reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "Can't find a image with that url" });
+                }
+        
+        
+            } else {
+                reject({ "status": "failed", "status_message": "can't resolve query", "discord_message": "invaled commmand:\nsearch name image_name\nsearch url image_url\nsearch id image_id" });
+        
+            }
+
+
+            break;
+        case "upload":
+
+
+
+            break;
+        case "tags":
+
+
+
+            break;
+        case "random":
+
+
+
+            break;
+        case "delete":
+
+
+
+            break;
+        case "edit":
+
+
+
+            break;
+        case "stats":
+
+
+
+            break;
+        case "actions":
+
+
+    }
+
 }
 
 exports.post = req => {
@@ -82,3 +169,8 @@ exports.post = req => {
 }
 
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
